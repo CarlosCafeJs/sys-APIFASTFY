8d1b32c453f6217c2d208f8518fd805de3dc3f1b
Carlos Henrique dos Santos <129197782+CarlosCafeJs@users.noreply.github.com>
2025-05-05 12:20:11 +0000

    Rota delete

---
+++ src/http/routes/delete-books.ts
@@ -0,0 +1,27 @@
+import { FastifyInstance } from 'fastify';
+import { z } from 'zod';
+import { prisma } from '../lib/prisma'
+
+export async function deleteBooks(app: FastifyInstance) {
+  app.delete('books/:bookId', async (request, reply) => {
+    const deleteBooksParams = z.object({
+      bookId: z.string().uuid(),
+    })
+    const { bookId } = deleteBooksParams.parse(request.params);
+
+    const book = await prisma.books.findUnique({
+      where: {
+        id: bookId,
+      },
+    })
+    if (!book) {
+      return reply.status(404).send({ message: "Livro Não Encontrado" })
+    }
+
+    await prisma.books.delete({
+      where: {
+        id: bookId,
+      }
+    })
+  })
+}
--- src/http/server.ts
+++ src/http/server.ts
@@ -2,12 +2,14 @@ import fastify from "fastify";
 import { createBooks } from "./routes/create-books";
 import { getBooks } from "./routes/get-books";
 import { updateBooks } from "./routes/update-books";
+import { deleteBooks } from "./routes/delete-books";
 
 const app = fastify();
 
 app.register(createBooks)
 app.register(getBooks)
 app.register(updateBooks)
+app.register(deleteBooks)
 
 
 app.listen({ port: 8080 }, () => {
